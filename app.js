9a0c0d97a13841628ac29dee32e464d74751a5d3
JakabGabor95 <trexgab648@gmail.com>
2023-04-10 21:29:24 +0200

    body parser

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
+//Middleware
+/* app.use((req, res, next) => {
   console.log("object");
   next();
+}); */
+
+/* app.use("/", (req, res, next) => {
+  console.log("This always run");
+  next();
+});
+ */
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submbit">add</button>The add product page</form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("object2");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from express</h1>");
 });
 
