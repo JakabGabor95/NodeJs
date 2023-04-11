76527d6057c133a34df8a7945bae907644ae9386
JakabGabor95 <trexgab648@gmail.com>
2023-04-11 17:26:47 +0200

    fix add product

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 //Middleware
@@ -21,7 +21,7 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 const rootDir = require("../util/path");
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
