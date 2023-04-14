fcee1675c9a72e9250e4b866243ab52d7df07dc7
JakabGabor95 <trexgab648@gmail.com>
2023-04-14 16:20:05 +0200

    add product model

---
+++ models/product.js
@@ -0,0 +1,15 @@
+const fs = requ;
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    products.push(this);
+  }
+
+  static fetchAll() {
+    return products;
+  }
+};
